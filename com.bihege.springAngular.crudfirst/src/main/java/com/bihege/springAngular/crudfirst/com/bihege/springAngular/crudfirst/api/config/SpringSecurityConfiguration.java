0ba1caed1c0b301ec501ba0a0dec2ee4ac2c93fb
YannickBihege <yannickbtheone@hotmail.com>
2022-09-30 12:16:02 +0200

    Upload configuration files in the remote repository

--- com.bihege.springAngular.crudfirst/src/main/java/com/bihege/springAngular/crudfirst/com/bihege/springAngular/crudfirst/api/config/SpringSecurityConfiguration.java
+++ com.bihege.springAngular.crudfirst/src/main/java/com/bihege/springAngular/crudfirst/com/bihege/springAngular/crudfirst/api/config/SpringSecurityConfiguration.java
@@ -4,7 +4,9 @@
         import org.springframework.context.annotation.ComponentScan;
         import org.springframework.context.annotation.Configuration;
         import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+        import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
         import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+        import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
         import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
         import org.springframework.security.core.userdetails.UserDetailsService;
         import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,6 +14,8 @@
 
         @ComponentScan
         @Configuration
+        @EnableWebSecurity
+        @EnableGlobalMethodSecurity(prePostEnabled = true)
         public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
             @Autowired
